2acf92374fdac1edca394e4794c5543f515487f0
isaantepli <isaantepli46@gmail.com>
2020-10-31 08:42:42 -0600

    Commiting the Gmail project

---
+++ src/Test/SendGmail.java
@@ -0,0 +1,30 @@
+package Test;
+import Library.Util;
+public class SendGmail {
+    /*
+            first Step:
+                go to  :
+                     https://myaccount.google.com/lesssecureapps?utm_source=google-account&utm_medium=web
+                        Make sure that your google account allows third part app
+ */
+    public static void main(String[] args) {
+        // credentials
+        String userName = "isaantepli46@gmail.com";
+        String passWord = "Hopepassenger7";
+        // email:
+        String[] receivers = {"anteplinelli@gmail.com"};
+        String subject = "Java Test";
+        String text = "I love you";
+        int times = 5;
+        int count  = 1;
+        while(times > 0){ //send the email multiple time
+            for( String each : receivers ){  // for sending emails to multiple people
+                System.out.println("Sending to: "+each  );
+                Util.sendEmails(userName,passWord,each, subject,text);
+            }
+            System.out.println(count++);
+            times--;
+        }
+        System.out.println("Completed");
+    }
+}
